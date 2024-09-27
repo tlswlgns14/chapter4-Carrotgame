8e55ac9b8298502d6dcaa36fa5a95510151ddd5e
tlswlgns14 <85921909+tlswlgns14@users.noreply.github.com>
2024-09-27 14:05:10 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,37 @@
+'use strict';
+import PopUp from './popup.js';
+import {GameBuilder, Reason} from './game.js';
+import * as sound from './sound.js';
+
+const gameFinishBanner = new PopUp();
+const game = new GameBuilder()
+.withGameDuration(5)
+.whitCarrotCount(3)
+.withBugCount(3)
+.build();
+
+game.setGameStopListener((reason) => {
+  console.log(reason);
+  let message;
+  switch (reason) {
+    case Reason.cancel:
+      message = 'Replay❓';
+      sound.playAlert();
+      break;
+    case Reason.win:
+      message = 'YOU WON 🎉';
+      sound.playWin();
+      break;
+    case Reason.lose:
+      message = 'YOU LOST 💩';
+      sound.playBug();
+      break;
+    default:
+      throw new Error('not valid reason');
+  }
+  gameFinishBanner.showWithText(message);
+});
+
+gameFinishBanner.setClickListener(() => {
+  game.start();
+});
